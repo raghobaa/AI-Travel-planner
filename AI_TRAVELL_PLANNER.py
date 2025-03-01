766a11d9d3d7278e28104d490c8aa5437c90d6da
abhiram <95032188+Abhiram4u@users.noreply.github.com>
2025-03-02 00:19:32 +0530

    Update AI_TRAVELL_PLANNER.py

--- AI_TRAVELL_PLANNER.py
+++ AI_TRAVELL_PLANNER.py
@@ -12,8 +12,9 @@ API_KEY = os.getenv("API_KEY")
 # Streamlit UI 
 st.set_page_config(page_title="AI-Powered Travel Planner", page_icon="\U0001F30D", layout="centered")
 
-image_url = "https://github.com/Abhiram4u/AI-Travel-planner/blob/main/travel%200.png?raw=true"
-st.image(image_url, use_container_width=True)  # Homepage image
+image_url = "https://raw.githubusercontent.com/Abhiram4u/AI-Travel-planner/main/travel%200.png"
+st.image(image_url, use_column_width=True) # homepage image
+
 
 
 st.markdown("<h1 style='text-align: center;'>AI-Powered Travel Planner</h1>", unsafe_allow_html=True)
